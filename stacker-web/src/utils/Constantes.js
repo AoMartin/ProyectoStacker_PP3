c56cc82473cd7b3b4d9f4882734ff415ecb35b33
Martin Ao <martin.andres.olsson@gmail.com>
2022-12-05 22:07:27 -0300

    stacker- agregar logs al gitignore

--- stacker-web/src/utils/Constantes.js
+++ stacker-web/src/utils/Constantes.js
@@ -1,6 +1,4 @@
-console.log(process.env.REACT_APP_API_URL)
-var max_val = process.env.REACT_APP_API_URL;
-console.log(max_val);
+
 export const API_URL = process.env.REACT_APP_API_URL;
 
 export const CONTROLLER_PUBLICACION = `/publicacion`;
